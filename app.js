aa6c989fa754fae51b14b0976f514a682bd13193
YasuGoto <yasuko.goto@nijibox.co.jp>
2025-01-15 23:18:45 +0900

    30mのとこまで

---
+++ app.js
@@ -0,0 +1,27 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //Time Display
+    const timeDisplay =document.querySelector('.time-display');
+    //Get the Length oof the outline
+    const outlineLength = outline.getTotalLength();
+    //Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = 200;
+    console.log(1111)
+
+    //play sound
+    play.addEventListener('click', ()=> {
+        song.play();
+    });
+}
+
+
+app();
